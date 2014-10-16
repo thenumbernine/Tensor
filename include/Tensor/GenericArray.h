eb063df62e6effc4c4809e5dd0b6161db88c605c
Christopher E. Moore <christopher.e.moore@gmail.com>
2014-10-15 19:20:10 -0700

    moved the meta stuff I added into a new file

--- include/Tensor/GenericArray.h
+++ include/Tensor/GenericArray.h
@@ -1,130 +1,9 @@
 #pragma once
 
 #include "Tensor/clamp.h"
-#include "Common/Meta.h"
+#include "Tensor/Meta.h"
 #include "Common/crtp_cast.h"
 #include <iostream>
-
-//assignment binary operations
-template<typename DstType, typename SrcType> struct Assign { static void exec(DstType& dst, const SrcType& src) { dst = src; } };
-template<typename DstType, typename SrcType> struct AddInto { static void exec(DstType& dst, const SrcType& src) { dst += src; } };
-template<typename DstType, typename SrcType> struct SubInto { static void exec(DstType& dst, const SrcType& src) { dst -= src; } };
-template<typename DstType, typename SrcType> struct MulInto { static void exec(DstType& dst, const SrcType& src) { dst *= src; } };
-template<typename DstType, typename SrcType> struct DivInto { static void exec(DstType& dst, const SrcType& src) { dst /= src; } };
-template<typename DstType, typename SrcType> struct AndInto { static void exec(DstType& dst, const SrcType& src) { dst &= src; } };
-template<typename DstType, typename SrcType> struct OrInto { static void exec(DstType& dst, const SrcType& src) { dst |= src; } };
-template<typename DstType, typename SrcType> struct XOrInto { static void exec(DstType& dst, const SrcType& src) { dst ^= src; } };
-
-//unary operations
-template<typename Type> struct Identity { static const Type &exec(const Type& a) { return a; } };
-template<typename Type> struct UnaryMinus { static Type exec(const Type& a) { return -a; } };
-
-template<typename Type> struct Add { static Type exec(const Type& a, const Type& b) { return a + b; } };
-template<typename Type> struct Sub { static Type exec(const Type& a, const Type& b) { return a - b; } };
-template<typename Type> struct Mul { static Type exec(const Type& a, const Type& b) { return a * b; } };
-template<typename Type> struct Div { static Type exec(const Type& a, const Type& b) { return a / b; } };
-template<typename Type> struct Equals { static bool exec(const Type& a, const Type& b) { return a == b; } };
-template<typename Type> struct NotEquals { static bool exec(const Type& a, const Type& b) { return a != b; } };
-template<typename Type> struct GreaterThan { static bool exec(const Type& a, const Type& b) { return a > b; } };
-template<typename Type> struct GreaterThanOrEquals { static bool exec(const Type& a, const Type& b) { return a >= b; } };
-template<typename Type> struct LessThan { static bool exec(const Type& a, const Type& b) { return a < b; } };
-template<typename Type> struct LessThanOrEquals { static bool exec(const Type& a, const Type& b) { return a <= b; } };
-struct LogicalAnd { static bool exec(bool a, bool b) { return a && b; } };
-struct LogicalOr { static bool exec(bool a, bool b) { return a || b; } };
-
-//functor wrapper for static function
-template<typename Type> struct Clamp { static Type exec(const Type& a, const Type& b, const Type& c) { return clamp(a,b,c); } };
-
-//accessing indexes
-
-template<typename Type> 
-struct ScalarAccess { 
-	typedef Type& Arg;
-	template<int index>
-	struct Inner {
-		static Type& exec(Type& src) { return src; }
-	};
-};
-
-template<typename Type> 
-struct ConstScalarAccess { 
-	typedef const Type& Arg;
-	template<int index> 
-	struct Inner {
-		static const Type& exec(const Type& src) { return src; }
-	};
-};
-
-template<typename Type> 
-struct ArrayAccess { 
-	typedef Type* Arg;
-	template<int index> 
-	struct Inner {
-		static Type& exec(Type* src) { return src[index]; }
-	};
-};
-
-template<typename Type>
-struct ConstArrayAccess { 
-	typedef const Type* Arg;
-	template<int index> 
-	struct Inner {
-		static const Type& exec(const Type* src) { return src[index]; }
-	};
-};
-
-//various operations
-	
-template<typename AssignOp, typename Func, typename AccessDest, typename AccessA>
-struct UnaryOp {
-	template<int index>
-	struct Inner {
-		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg src) {
-			AssignOp::exec(
-				AccessDest::template Inner<index>::exec(dst), 
-				Func::exec(
-					AccessA::template Inner<index>::exec(src)
-				)
-			);
-			return false;
-		}
-	};
-};
-
-template<typename AssignOp, typename Func, typename AccessDest, typename AccessA, typename AccessB>
-struct BinaryOp {
-	template<int index>
-	struct Inner {
-		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg a, typename AccessB::Arg b) {
-			AssignOp::exec(
-				AccessDest::template Inner<index>::exec(dst), 
-				Func::exec(
-					AccessA::template Inner<index>::exec(a), 
-					AccessB::template Inner<index>::exec(b)
-				)
-			);
-			return false;
-		}
-	};
-};
-
-template<typename AssignOp, typename Func, typename AccessDest, typename AccessA, typename AccessB, typename AccessC>
-struct TernaryOp {
-	template<int index>
-	struct Inner {
-		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg a, typename AccessB::Arg b, typename AccessC::Arg c) {
-			AssignOp::exec(
-				AccessDest::template Inner<index>::exec(dst),
-				Func::exec(
-					AccessA::template Inner<index>::exec(a),
-					AccessB::template Inner<index>::exec(b),
-					AccessC::template Inner<index>::exec(c)
-				)
-			);
-			return false;
-		}
-	};
-};
 
 namespace Tensor {
 
@@ -184,7 +63,7 @@ struct GenericArray {
 	template<typename ChildType2>
 	bool operator==(const GenericArray<Type, size, ScalarType, ChildType2> &b) const {
 		bool result = true;
-		ForLoop<0, size, BinaryOp<AndInto<bool, bool>, Equals<Type>, ScalarAccess<bool>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(result, v, b.v);
+		ForLoop<0, size, BinaryOp<AndInto<bool, bool>, Equals<bool, Type, Type>, ScalarAccess<bool>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(result, v, b.v);
 		return result;
 	}
 
@@ -202,13 +81,13 @@ struct GenericArray {
 
 	Child operator+(const Child &b) const {
 		Child c;
-		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Add<Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(c.v, v, b.v);
+		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Add<Type, Type, Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(c.v, v, b.v);
 		return c;
 	}
 	
 	Child operator-(const Child &b) const {
 		Child c;
-		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Sub<Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(c.v, v, b.v);
+		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Sub<Type, Type, Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstArrayAccess<Type>>::template Inner>::exec(c.v, v, b.v);
 		return c;
 	}
 
@@ -221,13 +100,13 @@ struct GenericArray {
 
 	Child operator*(const ScalarType &b) const {
 		Child c;
-		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Mul<Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstScalarAccess<Type>>::template Inner>::exec(c.v, v, b);
+		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Mul<Type, Type, ScalarType>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstScalarAccess<ScalarType>>::template Inner>::exec(c.v, v, b);
 		return c;
 	}
 
 	Child operator/(const ScalarType &b) const {
 		Child c;
-		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Div<Type>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstScalarAccess<Type>>::template Inner>::exec(c.v, v, b);
+		ForLoop<0, size, BinaryOp<Assign<Type, Type>, Div<Type, Type, ScalarType>, ArrayAccess<Type>, ConstArrayAccess<Type>, ConstScalarAccess<ScalarType>>::template Inner>::exec(c.v, v, b);
 		return c;
 	}
 
---
+++ include/Tensor/Meta.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "Common/Meta.h"
+
+namespace Tensor {
+
+//assignment binary operations
+template<typename DstType, typename SrcType> struct Assign { static void exec(DstType& dst, const SrcType& src) { dst = src; } };
+template<typename DstType, typename SrcType> struct AddInto { static void exec(DstType& dst, const SrcType& src) { dst += src; } };
+template<typename DstType, typename SrcType> struct SubInto { static void exec(DstType& dst, const SrcType& src) { dst -= src; } };
+template<typename DstType, typename SrcType> struct MulInto { static void exec(DstType& dst, const SrcType& src) { dst *= src; } };
+template<typename DstType, typename SrcType> struct DivInto { static void exec(DstType& dst, const SrcType& src) { dst /= src; } };
+template<typename DstType, typename SrcType> struct AndInto { static void exec(DstType& dst, const SrcType& src) { dst &= src; } };
+template<typename DstType, typename SrcType> struct OrInto { static void exec(DstType& dst, const SrcType& src) { dst |= src; } };
+template<typename DstType, typename SrcType> struct XOrInto { static void exec(DstType& dst, const SrcType& src) { dst ^= src; } };
+
+//unary operations
+template<typename Type> struct Identity { static const Type &exec(const Type& a) { return a; } };
+template<typename Type> struct UnaryMinus { static Type exec(const Type& a) { return -a; } };
+
+template<typename C, typename A, typename B> struct Add { static C exec(const A& a, const B& b) { return a + b; } };
+template<typename C, typename A, typename B> struct Sub { static C exec(const A& a, const B& b) { return a - b; } };
+template<typename C, typename A, typename B> struct Mul { static C exec(const A& a, const B& b) { return a * b; } };
+template<typename C, typename A, typename B> struct Div { static C exec(const A& a, const B& b) { return a / b; } };
+template<typename C, typename A, typename B> struct Equals { static C exec(const A& a, const B& b) { return a == b; } };
+template<typename C, typename A, typename B> struct NotEquals { static C exec(const A& a, const B& b) { return a != b; } };
+template<typename C, typename A, typename B> struct GreaterThan { static C exec(const A& a, const B& b) { return a > b; } };
+template<typename C, typename A, typename B> struct GreaterThanOrEquals { static C exec(const A& a, const B& b) { return a >= b; } };
+template<typename C, typename A, typename B> struct LessThan { static C exec(const A& a, const B& b) { return a < b; } };
+template<typename C, typename A, typename B> struct LessThanOrEquals { static C exec(const A& a, const B& b) { return a <= b; } };
+struct LogicalAnd { static bool exec(bool a, bool b) { return a && b; } };
+struct LogicalOr { static bool exec(bool a, bool b) { return a || b; } };
+
+//functor wrapper for static function
+template<typename Type> struct Clamp { static Type exec(const Type& a, const Type& b, const Type& c) { return clamp(a,b,c); } };
+
+//accessing indexes
+
+template<typename Type> 
+struct ScalarAccess { 
+	typedef Type& Arg;
+	template<int index>
+	struct Inner {
+		static Type& exec(Type& src) { return src; }
+	};
+};
+
+template<typename Type> 
+struct ConstScalarAccess { 
+	typedef const Type& Arg;
+	template<int index> 
+	struct Inner {
+		static const Type& exec(const Type& src) { return src; }
+	};
+};
+
+template<typename Type> 
+struct ArrayAccess { 
+	typedef Type* Arg;
+	template<int index> 
+	struct Inner {
+		static Type& exec(Type* src) { return src[index]; }
+	};
+};
+
+template<typename Type>
+struct ConstArrayAccess { 
+	typedef const Type* Arg;
+	template<int index> 
+	struct Inner {
+		static const Type& exec(const Type* src) { return src[index]; }
+	};
+};
+
+//various operations
+	
+template<typename AssignOp, typename Func, typename AccessDest, typename AccessA>
+struct UnaryOp {
+	template<int index>
+	struct Inner {
+		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg src) {
+			AssignOp::exec(
+				AccessDest::template Inner<index>::exec(dst), 
+				Func::exec(
+					AccessA::template Inner<index>::exec(src)
+				)
+			);
+			return false;
+		}
+	};
+};
+
+template<typename AssignOp, typename Func, typename AccessDest, typename AccessA, typename AccessB>
+struct BinaryOp {
+	template<int index>
+	struct Inner {
+		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg a, typename AccessB::Arg b) {
+			AssignOp::exec(
+				AccessDest::template Inner<index>::exec(dst), 
+				Func::exec(
+					AccessA::template Inner<index>::exec(a), 
+					AccessB::template Inner<index>::exec(b)
+				)
+			);
+			return false;
+		}
+	};
+};
+
+template<typename AssignOp, typename Func, typename AccessDest, typename AccessA, typename AccessB, typename AccessC>
+struct TernaryOp {
+	template<int index>
+	struct Inner {
+		static bool exec(typename AccessDest::Arg dst, typename AccessA::Arg a, typename AccessB::Arg b, typename AccessC::Arg c) {
+			AssignOp::exec(
+				AccessDest::template Inner<index>::exec(dst),
+				Func::exec(
+					AccessA::template Inner<index>::exec(a),
+					AccessB::template Inner<index>::exec(b),
+					AccessC::template Inner<index>::exec(c)
+				)
+			);
+			return false;
+		}
+	};
+};
+
+};
+
