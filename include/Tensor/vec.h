adbac88bbf86a405837bcd56ba37a9b69d8b54ed
Christopher Moore <christopher.e.moore@gmail.com>
2022-09-27 14:12:58 +0700

    got det and inv for sym

--- include/Tensor/vec.h
+++ include/Tensor/vec.h
@@ -7,6 +7,7 @@ NEW VERSION
 - no more lower and upper
 - modeled around glsl
 - maybe extensions into matlab syntax (nested tensor implicit * contraction mul)
+- math indexing.  A.i.j := a_ij, which means row-major storage (sorry OpenGL)
 
 TODO:
 	symmetric and antisymmetric matrices
@@ -847,145 +848,6 @@ _mat<T,dim2,dim1> transpose(_mat<T,dim1,dim2> const & a) {
 	return at;
 }
 
-// determinant
-// TODO generalize or at least expose Levi-Civita tensor as constexpr 
-// for implementing cross in higher dimensions, determinant, and whatever else.
-
-template<typename T, int dim>
-T determinant(_mat<T,dim,dim> const & a);
-
-template<typename T>
-T determinant(_mat2x2<T> const & a) {
-	return a.x.x * a.y.y - a.x.y * a.y.x;
-}
-template<typename T>
-T determinant(_mat3x3<T> const & a) {
-	return a.x.x * a.y.y * a.z.z
-		+ a.y.x * a.z.y * a.x.z
-		+ a.z.x * a.x.y * a.y.z
-		- a.x.z * a.y.y * a.z.x
-		- a.y.z * a.z.y * a.x.x
-		- a.z.z * a.x.y * a.y.x;
-}
-
-template<typename T>
-T determinant(_mat4x4<T> const & a) {
-	//autogen'd with symmath
-	T const tmp1 = a.s2.s2 * a.s3.s3;
-	T const tmp2 = a.s2.s3 * a.s3.s2;
-	T const tmp3 = a.s2.s1 * a.s3.s3;
-	T const tmp4 = a.s2.s3 * a.s3.s1;
-	T const tmp5 = a.s2.s1 * a.s3.s2;
-	T const tmp6 = a.s2.s2 * a.s3.s1;
-	T const tmp7 = a.s2.s0 * a.s3.s3;
-	T const tmp8 = a.s2.s3 * a.s3.s0;
-	T const tmp9 = a.s2.s0 * a.s3.s2;
-	T const tmp10 = a.s2.s2 * a.s3.s0;
-	T const tmp11 = a.s2.s0 * a.s3.s1;
-	T const tmp12 = a.s2.s1 * a.s3.s0;
-	return a.s0.s0 * a.s1.s1 * tmp1 
-		- a.s0.s0 * a.s1.s1 * tmp2 
-		- a.s0.s0 * a.s1.s2 * tmp3
-		+ a.s0.s0 * a.s1.s2 * tmp4
-		+ a.s0.s0 * a.s1.s3 * tmp5 
-		- a.s0.s0 * a.s1.s3 * tmp6 
-		- a.s0.s1 * a.s1.s0 * tmp1
-		+ a.s0.s1 * a.s1.s0 * tmp2
-		+ a.s0.s1 * a.s1.s2 * tmp7 
-		- a.s0.s1 * a.s1.s2 * tmp8 
-		- a.s0.s1 * a.s1.s3 * tmp9
-		+ a.s0.s1 * a.s1.s3 * tmp10
-		+ a.s0.s2 * a.s1.s0 * tmp3 
-		- a.s0.s2 * a.s1.s0 * tmp4 
-		- a.s0.s2 * a.s1.s1 * tmp7
-		+ a.s0.s2 * a.s1.s1 * tmp8
-		+ a.s0.s2 * a.s1.s3 * tmp11 
-		- a.s0.s2 * a.s1.s3 * tmp12 
-		- a.s0.s3 * a.s1.s0 * tmp5
-		+ a.s0.s3 * a.s1.s0 * tmp6
-		+ a.s0.s3 * a.s1.s1 * tmp9 
-		- a.s0.s3 * a.s1.s1 * tmp10
-		+ a.s0.s3 * a.s1.s2 * tmp12 
-		- a.s0.s3 * a.s1.s2 * tmp11;
-}
-
-// inverse
-
-template<typename T, int dim>
-_mat<T,dim,dim> inverse(_mat<T,dim,dim> const & a, T const det);
-
-template<typename T>
-_mat3x3<T> inverse(_mat3x3<T> const & a, T const det) {
-	T const invdet = T(1) / det;
-	return {
-		{
-			invdet * (-a.s1.s2 * a.s2.s1 + a.s1.s1 * a.s2.s2),
-			invdet * (a.s0.s2 * a.s2.s1 + -a.s0.s1 * a.s2.s2),
-			invdet * (-a.s0.s2 * a.s1.s1 + a.s0.s1 * a.s1.s2)
-		}, {
-			invdet * (a.s1.s2 * a.s2.s0 + -a.s1.s0 * a.s2.s2),
-			invdet * (-a.s0.s2 * a.s2.s0 + a.s0.s0 * a.s2.s2),
-			invdet * (a.s0.s2 * a.s1.s0 + -a.s0.s0 * a.s1.s2)
-		}, {
-			invdet * (-a.s1.s1 * a.s2.s0 + a.s1.s0 * a.s2.s1),
-			invdet * (a.s0.s1 * a.s2.s0 + -a.s0.s0 * a.s2.s1),
-			invdet * (-a.s0.s1 * a.s1.s0 + a.s0.s0 * a.s1.s1)
-		}
-	};
-}
-
-//from : https://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
-template<typename T>
-_mat4x4<T> inverse(_mat4x4<T> const & a, T const det) {
-	T const a2323 = a.s2.s2 * a.s3.s3 - a.s2.s3 * a.s3.s2;
-	T const a1323 = a.s2.s1 * a.s3.s3 - a.s2.s3 * a.s3.s1;
-	T const a1223 = a.s2.s1 * a.s3.s2 - a.s2.s2 * a.s3.s1;
-	T const a0323 = a.s2.s0 * a.s3.s3 - a.s2.s3 * a.s3.s0;
-	T const a0223 = a.s2.s0 * a.s3.s2 - a.s2.s2 * a.s3.s0;
-	T const a0123 = a.s2.s0 * a.s3.s1 - a.s2.s1 * a.s3.s0;
-	T const a2313 = a.s1.s2 * a.s3.s3 - a.s1.s3 * a.s3.s2;
-	T const a1313 = a.s1.s1 * a.s3.s3 - a.s1.s3 * a.s3.s1;
-	T const a1213 = a.s1.s1 * a.s3.s2 - a.s1.s2 * a.s3.s1;
-	T const a2312 = a.s1.s2 * a.s2.s3 - a.s1.s3 * a.s2.s2;
-	T const a1312 = a.s1.s1 * a.s2.s3 - a.s1.s3 * a.s2.s1;
-	T const a1212 = a.s1.s1 * a.s2.s2 - a.s1.s2 * a.s2.s1;
-	T const a0313 = a.s1.s0 * a.s3.s3 - a.s1.s3 * a.s3.s0;
-	T const a0213 = a.s1.s0 * a.s3.s2 - a.s1.s2 * a.s3.s0;
-	T const a0312 = a.s1.s0 * a.s2.s3 - a.s1.s3 * a.s2.s0;
-	T const a0212 = a.s1.s0 * a.s2.s2 - a.s1.s2 * a.s2.s0;
-	T const a0113 = a.s1.s0 * a.s3.s1 - a.s1.s1 * a.s3.s0;
-	T const a0112 = a.s1.s0 * a.s2.s1 - a.s1.s1 * a.s2.s0;
-	T const invdet = T(1) / det;
-	return { 
-		{
-		   invdet *  (a.s1.s1 * a2323 - a.s1.s2 * a1323 + a.s1.s3 * a1223),
-		   invdet * -(a.s0.s1 * a2323 - a.s0.s2 * a1323 + a.s0.s3 * a1223),
-		   invdet *  (a.s0.s1 * a2313 - a.s0.s2 * a1313 + a.s0.s3 * a1213),
-		   invdet * -(a.s0.s1 * a2312 - a.s0.s2 * a1312 + a.s0.s3 * a1212),
-		}, {
-		   invdet * -(a.s1.s0 * a2323 - a.s1.s2 * a0323 + a.s1.s3 * a0223),
-		   invdet *  (a.s0.s0 * a2323 - a.s0.s2 * a0323 + a.s0.s3 * a0223),
-		   invdet * -(a.s0.s0 * a2313 - a.s0.s2 * a0313 + a.s0.s3 * a0213),
-		   invdet *  (a.s0.s0 * a2312 - a.s0.s2 * a0312 + a.s0.s3 * a0212),
-		}, {
-		   invdet *  (a.s1.s0 * a1323 - a.s1.s1 * a0323 + a.s1.s3 * a0123),
-		   invdet * -(a.s0.s0 * a1323 - a.s0.s1 * a0323 + a.s0.s3 * a0123),
-		   invdet *  (a.s0.s0 * a1313 - a.s0.s1 * a0313 + a.s0.s3 * a0113),
-		   invdet * -(a.s0.s0 * a1312 - a.s0.s1 * a0312 + a.s0.s3 * a0112),
-		}, {
-		   invdet * -(a.s1.s0 * a1223 - a.s1.s1 * a0223 + a.s1.s2 * a0123),
-		   invdet *  (a.s0.s0 * a1223 - a.s0.s1 * a0223 + a.s0.s2 * a0123),
-		   invdet * -(a.s0.s0 * a1213 - a.s0.s1 * a0213 + a.s0.s2 * a0113),
-		   invdet *  (a.s0.s0 * a1212 - a.s0.s1 * a0212 + a.s0.s2 * a0112),
-		}
-	};
-}
-
-template<typename T, int dim>
-_mat<T,dim,dim> inverse(_mat<T,dim,dim> const & a) {
-	return inverse(a, determinant(a));
-}
-
 template<typename T, int dim>
 _mat<T,dim,dim> diagonalMatrix(_vec<T,dim> const & v) {
 	_mat<T,dim,dim> a;
@@ -994,37 +856,6 @@ _mat<T,dim,dim> diagonalMatrix(_vec<T,dim> const & v) {
 	}
 	return a;
 }
-
-#if 1
-
-template<typename T, int dim, int... dims>
-struct NestedTensor;
-
-template<typename T, int dim>
-struct NestedTensor<T, dim> {
-	using tensor = _vec<T,dim>;
-};
-
-template<typename T, int dim, int... dims>
-struct NestedTensor {
-	using tensor = _vec<typename NestedTensor<T, dims...>::tensor, dim>;
-};
-
-template<typename T, int dim, int... dims>
-using _tensor = NestedTensor<T, dim, dims...>::tensor;
-
-#else
-
-template<typename T, int dim, int... dims>
-using _tensor;
-
-template<typename T, int dim>
-using _tensor<T, dim> = _vec<T, dim>;
-
-template<typename T, int dim, int... dims>
-using _tensor = _vec<_tensor<T, dims...>, dim>;
-
-#endif
 
 // symmetric matrices
 
@@ -1319,6 +1150,241 @@ TENSOR2_ADD_SYMMETRIC_MATRIX_SCALAR_OP(-)
 TENSOR2_ADD_SYMMETRIC_MATRIX_SCALAR_OP(*)
 TENSOR2_ADD_SYMMETRIC_MATRIX_SCALAR_OP(/)
 
+// how to name symmetric-optimized storage?
+
+// using float2s2 in place of float2x2 does gieve the hint, but might be hard to read
+// another option is floatSym2
+template<typename T> using _sym2 = _sym<T,2>;
+using bool2s2 = _sym2<bool>;
+using uchar2s2 = _sym2<unsigned char>;
+using int2s2 = _sym2<int>;
+using uint2s2 = _sym2<uint>;
+using float2s2 = _sym2<float>;
+using double2s2 = _sym2<double>;
+
+template<typename T> using _sym3 = _sym<T,3>;
+using bool3s3 = _sym3<bool>;
+using uchar3s3 = _sym3<unsigned char>;
+using int3s3 = _sym3<int>;
+using uint3s3 = _sym3<uint>;
+using float3s3 = _sym3<float>;
+using double3s3 = _sym3<double>;
+static_assert(std::is_same_v<typename float3s3::ScalarType, float>);
+
+
+// determinant for matrix
+// TODO generalize or at least expose Levi-Civita tensor as constexpr 
+// for implementing cross in higher dimensions, determinant, and whatever else.
+
+template<typename T>
+T det22elem(T const & a00, T const & a01, T const & a10, T const & a11) {
+	return a00 * a11 - a01 * a10;
+}
+
+template<typename T>
+typename T::ScalarType determinant22(T const & a) {
+	return det22elem(a(0,0), a(0,1), a(1,0), a(1,1));
+}
+
+template<typename T>
+typename T::ScalarType determinant33(T const & a) {
+	return a(0,0) * a(1,1) * a(2,2)
+		+ a(0,1) * a(1,2) * a(2,0)
+		+ a(0,2) * a(1,0) * a(2,1)
+		- a(0,2) * a(1,1) * a(2,0)
+		- a(0,1) * a(1,0) * a(2,2)
+		- a(0,0) * a(1,2) * a(2,1);
+}
+
+template<typename T>
+T::ScalarType determinant(T const & a);
+
+template<typename T>
+T determinant(_mat2x2<T> const & a) {
+	return determinant22(a);
+}
+
+template<typename T>
+T determinant(_mat3x3<T> const & a) {
+	return determinant33(a);
+}
+
+template<typename T>
+T determinant(_mat4x4<T> const & a) {
+	//autogen'd with symmath
+	T const tmp1 = a.s2.s2 * a.s3.s3;
+	T const tmp2 = a.s2.s3 * a.s3.s2;
+	T const tmp3 = a.s2.s1 * a.s3.s3;
+	T const tmp4 = a.s2.s3 * a.s3.s1;
+	T const tmp5 = a.s2.s1 * a.s3.s2;
+	T const tmp6 = a.s2.s2 * a.s3.s1;
+	T const tmp7 = a.s2.s0 * a.s3.s3;
+	T const tmp8 = a.s2.s3 * a.s3.s0;
+	T const tmp9 = a.s2.s0 * a.s3.s2;
+	T const tmp10 = a.s2.s2 * a.s3.s0;
+	T const tmp11 = a.s2.s0 * a.s3.s1;
+	T const tmp12 = a.s2.s1 * a.s3.s0;
+	return a.s0.s0 * a.s1.s1 * tmp1 
+		- a.s0.s0 * a.s1.s1 * tmp2 
+		- a.s0.s0 * a.s1.s2 * tmp3
+		+ a.s0.s0 * a.s1.s2 * tmp4
+		+ a.s0.s0 * a.s1.s3 * tmp5 
+		- a.s0.s0 * a.s1.s3 * tmp6 
+		- a.s0.s1 * a.s1.s0 * tmp1
+		+ a.s0.s1 * a.s1.s0 * tmp2
+		+ a.s0.s1 * a.s1.s2 * tmp7 
+		- a.s0.s1 * a.s1.s2 * tmp8 
+		- a.s0.s1 * a.s1.s3 * tmp9
+		+ a.s0.s1 * a.s1.s3 * tmp10
+		+ a.s0.s2 * a.s1.s0 * tmp3 
+		- a.s0.s2 * a.s1.s0 * tmp4 
+		- a.s0.s2 * a.s1.s1 * tmp7
+		+ a.s0.s2 * a.s1.s1 * tmp8
+		+ a.s0.s2 * a.s1.s3 * tmp11 
+		- a.s0.s2 * a.s1.s3 * tmp12 
+		- a.s0.s3 * a.s1.s0 * tmp5
+		+ a.s0.s3 * a.s1.s0 * tmp6
+		+ a.s0.s3 * a.s1.s1 * tmp9 
+		- a.s0.s3 * a.s1.s1 * tmp10
+		+ a.s0.s3 * a.s1.s2 * tmp12 
+		- a.s0.s3 * a.s1.s2 * tmp11;
+}
+
+// determinant for symmetric
+
+template<typename T>
+T determinant(_sym2<T> const & a) {
+	return determinant22(a);
+}
+
+template<typename T>
+T determinant(_sym3<T> const & a) {
+	return determinant33(a);
+}
+
+// inverse for matrix
+
+template<typename T>
+typename T::ScalarType inverse(
+	T const & a,
+	typename T::ScalarType const & det
+);
+
+template<typename T>
+_mat2x2<T> inverse(_mat2x2<T> const & a, T const & det) {
+	return {
+		{
+			 a.s1.s1 / det,
+			-a.s0.s1 / det,
+		},
+		{
+			-a.s1.s0 / det,
+			 a.s0.s0 / det,
+		}
+	};
+}
+
+template<typename T>
+_mat3x3<T> inverse(_mat3x3<T> const & a, T const & det) {
+	return {
+		{
+			(-a.s1.s2 * a.s2.s1 +  a.s1.s1 * a.s2.s2) / det,
+			( a.s0.s2 * a.s2.s1 + -a.s0.s1 * a.s2.s2) / det,
+			(-a.s0.s2 * a.s1.s1 +  a.s0.s1 * a.s1.s2) / det,
+		}, {
+			( a.s1.s2 * a.s2.s0 + -a.s1.s0 * a.s2.s2) / det,
+			(-a.s0.s2 * a.s2.s0 +  a.s0.s0 * a.s2.s2) / det,
+			( a.s0.s2 * a.s1.s0 + -a.s0.s0 * a.s1.s2) / det,
+		}, {
+			(-a.s1.s1 * a.s2.s0 +  a.s1.s0 * a.s2.s1) / det,
+			( a.s0.s1 * a.s2.s0 + -a.s0.s0 * a.s2.s1) / det,
+			(-a.s0.s1 * a.s1.s0 +  a.s0.s0 * a.s1.s1) / det,
+		}
+	};
+}
+
+//from : https://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
+template<typename T>
+_mat4x4<T> inverse(_mat4x4<T> const & a, T const & det) {
+	T const a2323 = a.s2.s2 * a.s3.s3 - a.s2.s3 * a.s3.s2;
+	T const a1323 = a.s2.s1 * a.s3.s3 - a.s2.s3 * a.s3.s1;
+	T const a1223 = a.s2.s1 * a.s3.s2 - a.s2.s2 * a.s3.s1;
+	T const a0323 = a.s2.s0 * a.s3.s3 - a.s2.s3 * a.s3.s0;
+	T const a0223 = a.s2.s0 * a.s3.s2 - a.s2.s2 * a.s3.s0;
+	T const a0123 = a.s2.s0 * a.s3.s1 - a.s2.s1 * a.s3.s0;
+	T const a2313 = a.s1.s2 * a.s3.s3 - a.s1.s3 * a.s3.s2;
+	T const a1313 = a.s1.s1 * a.s3.s3 - a.s1.s3 * a.s3.s1;
+	T const a1213 = a.s1.s1 * a.s3.s2 - a.s1.s2 * a.s3.s1;
+	T const a2312 = a.s1.s2 * a.s2.s3 - a.s1.s3 * a.s2.s2;
+	T const a1312 = a.s1.s1 * a.s2.s3 - a.s1.s3 * a.s2.s1;
+	T const a1212 = a.s1.s1 * a.s2.s2 - a.s1.s2 * a.s2.s1;
+	T const a0313 = a.s1.s0 * a.s3.s3 - a.s1.s3 * a.s3.s0;
+	T const a0213 = a.s1.s0 * a.s3.s2 - a.s1.s2 * a.s3.s0;
+	T const a0312 = a.s1.s0 * a.s2.s3 - a.s1.s3 * a.s2.s0;
+	T const a0212 = a.s1.s0 * a.s2.s2 - a.s1.s2 * a.s2.s0;
+	T const a0113 = a.s1.s0 * a.s3.s1 - a.s1.s1 * a.s3.s0;
+	T const a0112 = a.s1.s0 * a.s2.s1 - a.s1.s1 * a.s2.s0;
+	return { 
+		{
+		    (a.s1.s1 * a2323 - a.s1.s2 * a1323 + a.s1.s3 * a1223) / det,
+		   -(a.s0.s1 * a2323 - a.s0.s2 * a1323 + a.s0.s3 * a1223) / det,
+		    (a.s0.s1 * a2313 - a.s0.s2 * a1313 + a.s0.s3 * a1213) / det,
+		   -(a.s0.s1 * a2312 - a.s0.s2 * a1312 + a.s0.s3 * a1212) / det,
+		}, {
+		   -(a.s1.s0 * a2323 - a.s1.s2 * a0323 + a.s1.s3 * a0223) / det,
+		    (a.s0.s0 * a2323 - a.s0.s2 * a0323 + a.s0.s3 * a0223) / det,
+		   -(a.s0.s0 * a2313 - a.s0.s2 * a0313 + a.s0.s3 * a0213) / det,
+		    (a.s0.s0 * a2312 - a.s0.s2 * a0312 + a.s0.s3 * a0212) / det,
+		}, {
+		    (a.s1.s0 * a1323 - a.s1.s1 * a0323 + a.s1.s3 * a0123) / det,
+		   -(a.s0.s0 * a1323 - a.s0.s1 * a0323 + a.s0.s3 * a0123) / det,
+		    (a.s0.s0 * a1313 - a.s0.s1 * a0313 + a.s0.s3 * a0113) / det,
+		   -(a.s0.s0 * a1312 - a.s0.s1 * a0312 + a.s0.s3 * a0112) / det,
+		}, {
+		   -(a.s1.s0 * a1223 - a.s1.s1 * a0223 + a.s1.s2 * a0123) / det,
+		    (a.s0.s0 * a1223 - a.s0.s1 * a0223 + a.s0.s2 * a0123) / det,
+		   -(a.s0.s0 * a1213 - a.s0.s1 * a0213 + a.s0.s2 * a0113) / det,
+		    (a.s0.s0 * a1212 - a.s0.s1 * a0212 + a.s0.s2 * a0112) / det,
+		}
+	};
+}
+
+// inverse for symmetric
+// has a different # of written fields so might as well optimize for it
+
+template<typename T, int dim>
+_sym<T,dim> inverse(_sym<T,dim> const & a, T const & det);
+
+// TODO sym2 from mat2 
+template<typename T>
+_sym2<T> inverse(_sym2<T> const & a, T const & det) {
+	return {
+		 a(1,1) / det, // AInv(0,0)
+		-a(0,1) / det, // AInv(1,0)
+		 a(0,0) / det, // AInv(1,1)
+	};
+}
+
+template<typename T>
+_sym3<T> inverse(_sym3<T> const & a, T const & det) {
+	return {
+		det22elem(a(1,1), a(1,2), a(2,1), a(2,2)) / det,	// AInv(0,0)
+		det22elem(a(1,2), a(1,0), a(2,2), a(2,0)) / det,	// AInv(1,0)
+		det22elem(a(0,0), a(0,2), a(2,0), a(2,2)) / det,	// AInv(1,1)
+		det22elem(a(1,0), a(1,1), a(2,0), a(2,1)) / det,	// AInv(2,0)
+		det22elem(a(0,1), a(0,0), a(2,1), a(2,0)) / det,	// AInv(2,1)
+		det22elem(a(0,0), a(0,1), a(1,0), a(1,1)) / det,	// AInv(2,2)
+	};
+}
+
+
+// inverse without determinant
+
+template<typename T>
+T inverse(T const & a) {
+	return inverse(a, determinant(a));
+}
+
 
 // ostream
 // _vec does have .fields
@@ -1351,6 +1417,40 @@ std::ostream & operator<<(std::ostream & o, _sym<Type,dim> const & t) {
 	return o;
 }
 #endif
+
+// tensor construction
+
+#if 1
+
+template<typename T, int dim, int... dims>
+struct NestedTensor;
+
+template<typename T, int dim>
+struct NestedTensor<T, dim> {
+	using tensor = _vec<T,dim>;
+};
+
+template<typename T, int dim, int... dims>
+struct NestedTensor {
+	using tensor = _vec<typename NestedTensor<T, dims...>::tensor, dim>;
+};
+
+template<typename T, int dim, int... dims>
+using _tensor = NestedTensor<T, dim, dims...>::tensor;
+
+#else
+
+template<typename T, int dim, int... dims>
+using _tensor;
+
+template<typename T, int dim>
+using _tensor<T, dim> = _vec<T, dim>;
+
+template<typename T, int dim, int... dims>
+using _tensor = _vec<_tensor<T, dims...>, dim>;
+
+#endif
+
 
 }
 }
