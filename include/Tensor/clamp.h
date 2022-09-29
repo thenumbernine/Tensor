9c0ea7802363c423cef9e51489a08ada050a8aa0
Christopher Moore <christopher.e.moore@gmail.com>
2022-09-29 19:27:58 +0700

    oops this never had a namespace.  was anyone even using it? I should make _tensor overloads for it.

--- include/Tensor/clamp.h
+++ include/Tensor/clamp.h
@@ -1,5 +1,7 @@
 #pragma once
 
+namespace Tensor {
+
 template<typename T>
 T& clamp(T &x, T &min, T &max) {
 	if (x < min) return min;
@@ -12,4 +14,6 @@ T const& clamp(T const &x, T const &min, T const &max) {
 	if (x < min) return min;
 	if (x > max) return max;
 	return x;
+}
+
 }
