8ad5d602d516ca0e9a9ee73dddc5ecebc5ce7bb2
Christopher Moore <christopher.e.moore@gmail.com>
2022-10-13 02:46:55 +0700

    ...which is here

---
+++ include/Tensor/Range.h
@@ -0,0 +1,123 @@
+#include <algorithm>
+
+namespace Tensor {
+
+template<int rank_>
+struct RangeObj {
+	static constexpr auto rank = rank_;
+	using intN = intN<rank>;
+	intN min, max;
+
+	RangeObj(intN min_, intN max_) : min(min_), max(max_) {}
+
+	// iterators
+
+	struct iterator {
+		intN index, min, max;
+		
+		iterator()  {}
+		iterator(intN min_, intN max_) : index(min_), min(min_), max(max_) {}
+		iterator(iterator const &iter) : index(iter.index), min(iter.min), max(iter.max) {}
+		
+		bool operator==(iterator const &b) const { return index == b.index; }
+		bool operator!=(iterator const &b) const { return index != b.index; }
+
+		//converts index to int
+		int flatten() const {
+			int flatIndex = 0;
+			for (int i = rank - 1; i >= 0; --i) {
+				flatIndex *= max(i) - min(i);
+				flatIndex += index(i) - min(i);
+			}
+			return flatIndex;
+		}
+	
+		//converts int to index
+		void unflatten(int flatIndex) {
+			for (int i = 0; i < rank; ++i) {
+				int s = max(i) - min(i);
+				int n = flatIndex;
+				if (i < rank-1) n %= s;
+				index(i) = n + min(i);
+				flatIndex = (flatIndex - n) / s;
+			}
+		}
+
+		iterator &operator+=(int offset) {
+			unflatten(flatten() + offset);
+			return *this;
+		}
+
+		iterator &operator-=(int offset) {
+			unflatten(flatten() - offset);
+			return *this;
+		}
+
+		iterator operator+(int offset) const {
+			return iterator(*this) += offset;
+		}
+
+		iterator operator-(int offset) const {
+			return iterator(*this) -= offset;
+		}
+
+		int operator-(iterator const &i) const {
+			return flatten() - i.flatten();
+		}
+
+		iterator &operator++() {
+			for (int i = 0; i < rank; ++i) {	//allow the last index to overflow for sake of comparing it to end
+				++index(i);
+				if (index(i) < max(i)) break;
+				if (i < rank-1) index(i) = min(i);
+			}
+			return *this;
+		}
+
+		intN &operator*() { return index; }
+		intN *operator->() { return &index; }
+	};
+
+	iterator begin() {
+		return iterator(min, max);
+	}
+	iterator end() {
+		iterator i(min, max);
+		i.index(rank-1) = i.max(rank-1);
+		return i;
+	}
+	
+	struct const_iterator {
+		intN index, min, max;
+		
+		const_iterator() {}
+		const_iterator(intN min_, intN max_) : min(min_), max(max_) {}
+		const_iterator(const_iterator const &iter) : index(iter.index), min(iter.min), max(iter.max) {}
+		
+		bool operator==(const_iterator const &b) const { return index == b.index; }
+		bool operator!=(const_iterator const &b) const { return index != b.index; }
+		
+		const_iterator &operator++() {
+			for (int i = 0; i < rank; ++i) {	//allow the last index to overflow for sake of comparing it to end
+				++index(i);
+				if (index(i) < max(i)) break;
+				if (i < rank-1) index(i) = min(i);
+			}
+			return *this;
+		}
+
+		intN const &operator*() const { return index; }
+		intN const *operator->() const { return &index; }
+	};
+
+	const_iterator begin() const {
+		return const_iterator(min, max);
+	}
+	const_iterator end() const {
+		const_iterator i(min, max);
+		i.index(rank-1) = i.max(rank-1);
+		return i;
+	}
+};
+
+}
